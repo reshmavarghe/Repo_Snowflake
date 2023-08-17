c30f68acae83a00dfe0898aa898473c3350817ad
Reshma Varghese <reshma.varghese@ba.com>
2023-08-17 13:04:46 +0100

    Added

--- reshmas3snow/reshmas3snow/reshmas3snow_stack.py
+++ reshmas3snow/reshmas3snow/reshmas3snow_stack.py
@@ -11,17 +11,10 @@ class Reshmas3SnowStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Reshmas3SnowQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-        s3.Bucket(
+        bucket=s3.Bucket(
             self,
             id="reshmasnows3bucket",
             bucket_name="reshmasnows3bucket",
-            encyption=s3.BucketEncryption.KMS,
+            encryption=s3.BucketEncryption.KMS,
             bucket_key_enabled=True
                 )
